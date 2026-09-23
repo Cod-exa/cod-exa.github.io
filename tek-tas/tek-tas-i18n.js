828b4c825534e15b157c629889da47aa690cf9b3
Erdinç Topal <erdinctopal.et@gmail.com>
2026-09-24 00:11:57 +0300

    Use Peg Solitaire Maht Game site name

--- tek-tas/tek-tas-i18n.js
+++ tek-tas/tek-tas-i18n.js
@@ -3,6 +3,6 @@
   window.PAGE_TRANSLATIONS=window.PAGE_TRANSLATIONS||{};
   Object.assign(window.PAGE_TRANSLATIONS,{
     all_products:{tr:'Tüm ürünler ↗',en:'All products ↗',de:'Alle Produkte ↗',zh:'所有产品 ↗',hi:'सभी उत्पाद ↗',es:'Todos los productos ↗',fr:'Tous les produits ↗',ar:'جميع المنتجات ↗',pt:'Todos os produtos ↗',ru:'Все продукты ↗',id:'Semua produk ↗',bn:'সব পণ্য ↗',ur:'تمام پروڈکٹس ↗',ja:'すべての製品 ↗',ko:'모든 제품 ↗'},
-    tek_tas_legal_marker:{tr:'Tek Taş',en:'Peg Solitaire',de:'Englisches Solitär',zh:'单人跳棋',hi:'पेग सॉलिटेयर',es:'Solitario de clavijas',fr:'Solitaire anglais',ar:'سوليتير القفز',pt:'Resta Um',ru:'Английский солитер',id:'Solitaire Pasak',bn:'পেগ সলিটেয়ার',ur:'پیگ سولیٹیئر',ja:'ペグ・ソリティア',ko:'페그 솔리테어'}
+    tek_tas_legal_marker:{tr:'Peg Solitaire Maht Game',en:'Peg Solitaire Maht Game',de:'Peg Solitaire Maht Game',zh:'Peg Solitaire Maht Game',hi:'Peg Solitaire Maht Game',es:'Peg Solitaire Maht Game',fr:'Peg Solitaire Maht Game',ar:'Peg Solitaire Maht Game',pt:'Peg Solitaire Maht Game',ru:'Peg Solitaire Maht Game',id:'Peg Solitaire Maht Game',bn:'Peg Solitaire Maht Game',ur:'Peg Solitaire Maht Game',ja:'Peg Solitaire Maht Game',ko:'Peg Solitaire Maht Game'}
   });
 })();
